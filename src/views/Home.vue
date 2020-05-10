84c013b78744f32e654106fd846b47b3c5356ab9
Oskar Wiegaard <oskar.wiegaard@hotmail.com>
2020-05-10 14:15:26 +0200

    Revert "3"
    This reverts commit e12bb376634b5b0c38e362b2f984b5e8c1874ea8.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <post-list></post-list>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import PostList from '../components/PostList.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    PostList
+    HelloWorld
   }
 }
 </script>
-
-<style>
-
-
-
-</style>
